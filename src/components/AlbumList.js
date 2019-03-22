7c1d29a8a286e66e12e8240cda0dbc28818b7396
pferreira82 <peter.e.ferreira@gmail.com>
2019-03-22 12:06:14 -0400

    Created AlbumDetail

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,8 @@
 // Import libraries for making a component
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
     state = { albums: [] };
@@ -13,7 +14,7 @@ class AlbumList extends Component {
     
     renderAlbums() {
         return this.state.albums.map(album => 
-            <Text key={album.title}>{album.title}</Text>
+            <AlbumDetail key={album.title} album={album} />
         );
     }
 
