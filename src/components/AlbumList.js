e780decc9c0937dd61c68e233efc883288707903
pferreira82 <peter.e.ferreira@gmail.com>
2019-03-25 14:16:03 -0400

    Modified AlbumDetail

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,6 @@
 // Import libraries for making a component
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -22,9 +22,9 @@ class AlbumList extends Component {
         console.log(this.state);
         
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
